e965d53273bff0ca860ad41c1c9259b5c8067013
Abhishek Sharma <abhishekshaxma8356@gmail.com>
2022-09-16 22:39:39 +0530

    v1

--- App.js
+++ App.js
@@ -1,1 +1,29 @@
 console.log("App.js is connected");
+
+let slideImg = document.getElementsByClassName('reSize-img');
+console.log(slideImg);
+// for loading the image
+setInterval(() => {
+    let slideImg = document.getElementsByClassName('reSize-img');
+    slideImg[1].src='./super.assest/asset 13.jpeg'
+}, 2000)
+setInterval(() => {
+    let slideImg = document.getElementsByClassName('reSize-img');
+    slideImg[1].src='./super.assest/asset 14.jpeg'
+}, 4000)
+setInterval(() => {
+    let slideImg = document.getElementsByClassName('reSize-img');
+    slideImg[1].src='./super.assest/asset 15.jpeg'
+}, 6000)
+setInterval(() => {
+    let slideImg = document.getElementsByClassName('reSize-img');
+    slideImg[1].src='./super.assest/asset 12.jpeg'
+}, 8000)
+setInterval(() => {
+    let slideImg = document.getElementsByClassName('reSize-img');
+    slideImg[1].src='./super.assest/AIR.jpg'
+}, 10000)
+
+loading = () => {
+    
+}
